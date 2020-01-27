2b4bb4f00933c9d9b9122819c21e0df32ad06f96
Istiak Morsalin <anik.cse@gmail.com>
2020-01-27 22:51:08 +0600

    first canvas view is shown wow

--- app/src/main/java/com/istiak/canvastest/FirstCanvas.java
+++ app/src/main/java/com/istiak/canvastest/FirstCanvas.java
@@ -2,6 +2,9 @@ package com.istiak.canvastest;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,8 +12,13 @@ import androidx.annotation.Nullable;
 
 public class FirstCanvas extends View {
 
+    Paint paint;
+    Rect rect;
+
     public FirstCanvas(Context context) {
         super(context);
+        paint = new Paint();
+        rect = new Rect();
     }
 
     public FirstCanvas(Context context, @Nullable AttributeSet attrs) {
@@ -28,5 +36,9 @@ public class FirstCanvas extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(3);
+
+        canvas.drawRect(0, 0, canvas.getWidth()/2, canvas.getHeight()/2, paint);
     }
 }
